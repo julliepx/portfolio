417a9add727d2c668f07f4314d2dbe2e6468ffd6
Jullie <jlpaixaof@gmail.com>
2025-02-07 04:19:58 -0300

    fix: updating links and resume

--- app/components/regular/Footer/index.tsx
+++ app/components/regular/Footer/index.tsx
@@ -1,59 +1,59 @@
-'use client';
-import { useLanguageContext } from '@/app/context/LanguageContext'
-import GithubIcon from '../../icons/GithubIcon'
-import LinkedinIcon from '../../icons/LinkedinIcon'
-import MailIcon from '../../icons/MailIcon'
-import ResumeIcon from '../../icons/ResumeIcon'
-import './styles.scss'
+"use client";
+import { useLanguageContext } from "@/app/context/LanguageContext";
+import GithubIcon from "../../icons/GithubIcon";
+import LinkedinIcon from "../../icons/LinkedinIcon";
+import MailIcon from "../../icons/MailIcon";
+import ResumeIcon from "../../icons/ResumeIcon";
+import "./styles.scss";
 
 const Footer = () => {
-    const { texts } = useLanguageContext();
+  const { texts } = useLanguageContext();
 
-    return (
-        <footer className='footer-container' id='contacts'>
-            <div className='contacts-list'>
-                <a href="https://github.com/julliepx" target='_blank'>
-                    <div className='contact'>
-                        <div className='contact-cover'></div>
-                        <div className='contact-icon'><GithubIcon /></div>
-                        <span className='contact-name'>GitHub</span>
-                    </div>
-                </a>
-                <a href="https://linkedin.com/in/jlpaixaof" target='_blank'>
-                    <div className='contact'>
-                        <div className='contact-cover'></div>
-                        <div className='contact-icon'><LinkedinIcon /></div>
-                        <span className='contact-name'>LinkedIn</span>
-                    </div>
-                </a>
-                {/* <a href="jullieresume.pdf">
-                <div className='contact'>
-                    <div className='contact-cover'></div>
-                    <div className='contact-icon'><ResumeIcon /></div>
-                    <span className='contact-name'>Currículo</span>
-                </div>
-            </a> */}
-                <a href="">
-                    <div className='contact'>
-                        <div className='contact-cover'></div>
-                        <div className='contact-icon'><ResumeIcon /></div>
-                        <span className='contact-name'>CV: Soon</span>
-                    </div>
-                </a>
-                <a href="mailto:jlpaixaof@gmail.com">
-                    <div className='contact'>
-                        <div className='contact-cover'></div>
-                        <div className='contact-icon'><MailIcon /></div>
-                        <span className='contact-name'>E-mail</span>
-                    </div>
-                </a>
+  return (
+    <footer className="footer-container" id="contacts">
+      <div className="contacts-list">
+        <a href="https://github.com/julliepx" target="_blank">
+          <div className="contact">
+            <div className="contact-cover"></div>
+            <div className="contact-icon">
+              <GithubIcon />
             </div>
-            <div className='copyrights'>
-                <h4 className='copyrights-title'>{texts.footer.copyrightTitle}</h4>
-                <span className='copyrights-subtitle'>{texts.footer.copyrightSubtitle} <strong><a href="https://discord.gg/FZJ2ubWSa2">{texts.footer.copyrightStrong}</a></strong></span>
+            <span className="contact-name">GitHub</span>
+          </div>
+        </a>
+        <a href="https://linkedin.com/in/eujullie" target="_blank">
+          <div className="contact">
+            <div className="contact-cover"></div>
+            <div className="contact-icon">
+              <LinkedinIcon />
             </div>
-        </footer>
-    )
-}
+            <span className="contact-name">LinkedIn</span>
+          </div>
+        </a>
+        <a href="files/resume.pdf" target="_blank">
+          <div className="contact">
+            <div className="contact-cover"></div>
+            <div className="contact-icon">
+              <ResumeIcon />
+            </div>
+            <span className="contact-name">{texts.footer.resume}</span>
+          </div>
+        </a>
+        <a href="mailto:euujullie@gmail.com">
+          <div className="contact">
+            <div className="contact-cover"></div>
+            <div className="contact-icon">
+              <MailIcon />
+            </div>
+            <span className="contact-name">E-mail</span>
+          </div>
+        </a>
+      </div>
+      <div className="copyrights">
+        <h4 className="copyrights-title">{texts.footer.copyrightTitle}</h4>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
